5f125e4dddd9aede9ca14e3c28b8c7f6b98103c1
leofavre <contato@leofavre.com>
2018-10-14 18:38:54 -0300

    chore: Expose coercion methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import withReflection from './decorators/withReflection.js';
 import withWhenConnected from './decorators/withWhenConnected.js';
 
+import coerceToArray from './coercion/coerceToArray.js';
+import coerceToArrayOrUndefined from './coercion/coerceToArrayOrUndefined.js';
+import coerceToBoolean from './coercion/coerceToBoolean.js';
+import coerceToNumber from './coercion/coerceToNumber.js';
+import coerceToNumberOrUndefined from './coercion/coerceToNumberOrUndefined.js';
+import coerceToString from './coercion/coerceToString.js';
+import coerceToStringOrUndefined from './coercion/coerceToStringOrUndefined.js';
+
 export {
   withReflection,
-  withWhenConnected
+  withWhenConnected,
+  coerceToArray,
+  coerceToArrayOrUndefined,
+  coerceToBoolean,
+  coerceToNumber,
+  coerceToNumberOrUndefined,
+  coerceToString,
+  coerceToStringOrUndefined
 };
