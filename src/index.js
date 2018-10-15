acb50a9e85da50fbecd6abe4aadc9a9757f53fe0
leofavre <contato@leofavre.com>
2018-10-15 09:22:02 -0300

    feat: Implement `withDomEvents`
    Closes #11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import withElmnt from './decorators/withElmnt.js';
 import withSetState from './decorators/withSetState.js';
+import withDomEventsRaw from './decorators/withDomEvents.js';
 
 import coerceToArray from './coercion/coerceToArray.js';
 import coerceToArrayOrUndefined from './coercion/coerceToArrayOrUndefined.js';
@@ -9,9 +10,12 @@ import coerceToNumberOrUndefined from './coercion/coerceToNumberOrUndefined.js';
 import coerceToString from './coercion/coerceToString.js';
 import coerceToStringOrUndefined from './coercion/coerceToStringOrUndefined.js';
 
+const withDomEvents = withDomEventsRaw(window.CustomEvent);
+
 export {
   withElmnt,
   withSetState,
+  withDomEvents,
   coerceToArray,
   coerceToArrayOrUndefined,
   coerceToBoolean,
